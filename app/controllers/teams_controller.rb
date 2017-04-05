65cda9426f35bf12fff64a23d328c3348a2191a9
suilulu <m13869841793@gmail.com>
2017-04-05 18:52:13 +0800

    install devise

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,5 @@
 class TeamsController < ApplicationController
+  before_action :authenticate_user! , only: [:new]
   def index
     @teams =Team.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :teams
   root 'teams#index'
   resources :teams do
