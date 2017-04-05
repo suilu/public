61361eca3dca7274f7384a0e77560f86e0fcb03f
suilulu <m13869841793@gmail.com>
2017-04-05 17:25:59 +0800

    implement groups#destroy

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,10 +5,20 @@ class TeamsController < ApplicationController
   def new
     @team = Team.new
   end
+  def show
+    @team = Team.find(params[:id])
+  end
 
   def create
     @team = Team.new(team_params)
     @team.save
+    redirect_to teams_path
+  end
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    flash[:alert] = "Team deleted"
     redirect_to teams_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :teams
   root 'teams#index'
+  resources :teams do
+    resources :projects
+  end
 end
