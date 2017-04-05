5a1cbdecae4656319c7e8a3d43500a30d0645571
suilulu <m13869841793@gmail.com>
2017-04-05 19:17:08 +0800

    add project and hook team/user relation

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ApplicationRecord
   belongs_to :user
+  has_many :projects
   validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170405110003) do
+ActiveRecord::Schema.define(version: 20170405111236) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "team_id"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string   "title"
