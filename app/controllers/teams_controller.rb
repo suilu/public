db998481d0f7c4dbacf3b53525652f4afa121592
suilulu <m13869841793@gmail.com>
2017-04-05 16:41:06 +0800

    implement groups#create

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,18 @@ class TeamsController < ApplicationController
   def index
     @teams =Team.all
   end
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    @team.save
+    redirect_to teams_path
+  end
+
+  private
+  def team_params
+    params.require(:team).permit(:title)
+  end
 end
